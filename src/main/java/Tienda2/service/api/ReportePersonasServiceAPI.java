b6f89ca0367a99a6074e7c2429ec59d6e8446249
LAPTOP-1QAOIALS\anaso <jeffrysmorales@gmail.com>
2022-08-09 22:41:23 -0600

    Commit 9

---
+++ src/main/java/Tienda2/service/api/ReportePersonasServiceAPI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tienda2.service.api;
+import Tienda2.model.ReportePersonasDTO;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+import net.sf.jasperreports.engine.JRException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+
+
+public interface ReportePersonasServiceAPI {
+    
+    ReportePersonasDTO obtenerReportePersonas(Map<String, Object> params) throws JRException, IOException, SQLException;
+    
+}
